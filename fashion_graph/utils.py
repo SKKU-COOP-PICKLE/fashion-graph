8f6a0d4e63f6a8aeeb0540c0d3c87640bb77287f
Jiyeon Baek <whitedelay@gmail.com>
2021-02-24 22:44:44 +0900

    matrix shape 안맞는 부분 수정

--- fashion_graph/utils.py
+++ fashion_graph/utils.py
@@ -6,14 +6,14 @@ def expand_csr_adj(adj, count:int):
     Expand csr adj matrix by adding empty adj rows and columns
     ex. add 2 in (4,4) -> (6,6) with emtpy adj rows and columns
     
-    returns expanded adj csr matrix, expanded # of indexes 
+    returns expanded adj csr matrix
     """
     r,c = adj.shape
     
     adj = sp.vstack(
         [adj, sp.csr_matrix(np.zeros((count, c)))])
     adj = sp.hstack(
-        [adj, sp.csr_matrix(np.zeros((r, count)))])
+        [adj, sp.csr_matrix(np.zeros((r + count, count)))])
     
-    return adj, list(range(r,r+count))
+    return adj
 
